f10f30452bce0bea00d0a419e8babc264d1d6f62
Neil <neilkosh001@gmail.com>
2018-08-02 18:46:12 -0400

    added spotify

--- liri.js
+++ liri.js
@@ -2,59 +2,80 @@ var keys = require('./keys.js');
 
 var Twitter = require('twitter');
 
-var spotify = require('spotify');
+var Spotify = require('node-spotify-api');
 
-var getMyTweets = function (){
+var spotify = new Spotify(keys.spotifyKeys);
 
- 
-var client = new Twitter(keys.twitterKeys);
-var spotify = new Spotify(keys.spotify);
+var getMyTweets = function () {
 
 
- 
-var params = {screen_name: '_angrbrd'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
+  var client = new Twitter(keys.twitterKeys);
 
 
-for (var i=0; i<tweets.length; i++)  {
-console.log (tweets[i].created_at);
-console.log('');
-console.log(tweets[i].text);
-  
+  var params = { screen_name: '_angrbrd' };
+  client.get('statuses/user_timeline', params, function (error, tweets, response) {
+    if (!error) {
+
+
+      for (var i = 0; i < tweets.length; i++) {
+        console.log(tweets[i].created_at);
+        console.log(' ');
+        console.log(tweets[i].text);
+
+      }
     }
-  }
-});
+  });
 
 }
 
+var spotifyThis = function () {
 
-spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
-  if ( err ) {
+  spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function (err, data) {
+    if (err) {
       console.log('Error occurred: ' + err);
       return;
-  }
+    
+    };
 
-console.log(data);
+    console.log(data.tracks.items[0]);
 
-});
-
-
-var pick = function (caseData, functionData){
-  switch(caseData) {
-    case 'my-tweets':
-    getMyTweets();
-    break;
-    default:
-    console.log('LIRI does not know that');
+    for (var x = 0; x < 20; x++) {
+      console.log("Songs Information #" + (x + 1),
+        "\nName: " + data.tracks.items[x].name,
+        "\nArtist: " + data.tracks.items[x].artists[0].name,
+        "\nAlbum: " + data.tracks.items[x].album.name,
+        "\nPreview URL: " + data.tracks.items[x].preview_url + "\n");
     }
+  });
+
+
+};
+
+
+
+var pick = function (caseData, functionData) {
+  switch (caseData) {
+    case 'my-tweets':
+      getMyTweets();
+      break;
+    case 'my-spotify':
+      spotifyThis();
+      break;
+    default:
+      console.log('LIRI does not know that');
   }
+}
 
 
-var runThis = function(argOne, argTwo) {
-  pick(argOne , argTwo);
+var runThis = function (argOne, argTwo) {
+  pick(argOne, argTwo);
 };
 
 runThis(process.argv[2], process.argv[3]);
-      
-  
+
+
+
+
+
+
+
